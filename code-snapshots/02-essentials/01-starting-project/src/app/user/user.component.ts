720287a02e878df17887dd2f7ea6c8289235cb5f
Matthew Robertson <matthew2556@hotmail.com>
2025-09-07 15:13:01 -0400

    2.32

--- code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -11,14 +11,17 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  //@Input({required : true}) avatar!: string;
+  //@Input({required : true}) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
 
   //selectedUser = signal(DUMMY_USERS[randomIndex]);
   //imagePath = computed(()=>'assets/users/' + this.selectedUser().avatar);
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
+  imagePath = computed(()=>'assets/users/' + this.avatar());
+  //get imagePath() {
+  //  return 'assets/users/' + this.avatar;
+  //}
   onSelectUser() {
     //const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     //this.selectedUser.set(DUMMY_USERS[randomIndex]);
