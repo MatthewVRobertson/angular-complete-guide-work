ff870e5589d34e71d0bf20a084e7dd2339f9a36c
Matthew Robertson <matthew2556@hotmail.com>
2025-09-07 15:37:12 -0400

    2.36

--- code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, EventEmitter, Input, input, Output, signal } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, output, Output, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 //const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
@@ -14,7 +14,8 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   //@Input({required : true}) avatar!: string;
   //@Input({required : true}) name!: string;
-  @Output() select = new EventEmitter();
+  //@Output() select = new EventEmitter();
+  select = output<string>();
 
   avatar = input.required<string>();
   name = input.required<string>();
@@ -28,6 +29,7 @@ export class UserComponent {
   onSelectUser() {
     //const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     //this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    //this.select.emit(this.id);
     this.select.emit(this.id);
   }
 }
