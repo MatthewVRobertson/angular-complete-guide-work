e3519e3c4e6add6cbb42e24dd5336304a0733e56
Matthew Robertson <matthew2556@hotmail.com>
2025-09-09 19:44:06 -0400

    40

--- code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -3,6 +3,12 @@ import { DUMMY_USERS } from '../dummy-users';
 
 //const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
+
+type User = {
+      id: string,
+    name: string,
+    avatar: string
+}
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -11,25 +17,27 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
+  @Input({required:true}) user! : User;
+  @Output() select = new EventEmitter();
+
+  //@Input({required: true}) id!: string;
   //@Input({required : true}) avatar!: string;
   //@Input({required : true}) name!: string;
   //@Output() select = new EventEmitter();
-  select = output<string>();
-
-  avatar = input.required<string>();
-  name = input.required<string>();
-
+  //select = output<string>();
+  //avatar = input.required<string>();
+  //name = input.required<string>();
   //selectedUser = signal(DUMMY_USERS[randomIndex]);
   //imagePath = computed(()=>'assets/users/' + this.selectedUser().avatar);
-  imagePath = computed(()=>'assets/users/' + this.avatar());
-  //get imagePath() {
-  //  return 'assets/users/' + this.avatar;
-  //}
+  //imagePath = computed(()=>'assets/users/' + this.avatar());
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
   onSelectUser() {
     //const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     //this.selectedUser.set(DUMMY_USERS[randomIndex]);
     //this.select.emit(this.id);
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
